7d22ea7c010e580c08b58e4e0b36181073ab054d
Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2025-08-01 13:40:02 +0800

    chore: delete `packages/ui` folder & minor clean-up

--- apps/api/src/app/route.ts
+++ apps/api/src/app/route.ts
@@ -2,6 +2,6 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   return NextResponse.json({
-    message: `Hello world! ${process.env.NEXT_PUBLIC_HOST_URL}`,
+    message: 'Hello world!',
   });
 }
